b27085e1e7e77292b69ed762643fb7a19a9b8e6a
Copra24 <folefac24.c@gmail.com>
2024-08-09 20:16:28 +0100

    Testcase 9 - 12

---
+++ tests/URP_TC10.spec.js
@@ -0,0 +1,46 @@
+const { test, expect } = require('@playwright/test');
+const { navigate, fieldsLocators } = require('../PageObject/RegistrationPagePOM');
+const { UserData } = require('../FixturesFile/fixturesData');
+
+test.describe('Registration Page Test', () => {
+
+    const user = UserData();// Creates object of UserData
+
+    let dialogHandled = false;// Initialize a flag to track whether a dialog (such as an alert or confirmation) has been handled during the test
+    
+    test.beforeEach(async ({ page }) => {
+  
+      // Navigates to the registration page Url from the Pom
+      await navigate(page);
+      
+    });
+
+    test('Verify Last Name Field correctly rejects Special characters', async ({ page }) => {
+    const locators = await fieldsLocators(page);//Create object of the fieldsLocators
+
+    //Enter valid First name
+    await locators.firstName.fill(user.first_name[0])
+    //Enters invalid Last name
+     await locators.lastName.fill(user.last_name[4]);
+
+     // Handle the dialog that appears when the Last name field is filled with Special characters
+  page.on('dialog', async dialog => {
+    dialogHandled = true;
+    try {
+      // Assert the alert message
+      expect(dialog.message()).toBe('Last name must contain alphabetical characters only');
+      await dialog.accept();
+    } catch (error) {
+      console.error('Error handling the dialog:', error);
+      await dialog.dismiss(); 
+    }
+  });
+
+     //Submits the form
+     await locators.submitButton.click()
+
+    });
+
+
+
+});
